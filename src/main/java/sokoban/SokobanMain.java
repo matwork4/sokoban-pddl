7b01f9cc13ceb35eb3938efd122daf7fe4faff16
matwork4 <51155034+matwork4@users.noreply.github.com>
2022-10-01 19:18:13 +0200

    Sokoban first push

---
+++ src/main/java/sokoban/SokobanMain.java
@@ -0,0 +1,13 @@
+package sokoban;
+
+import com.codingame.gameengine.runner.SoloGameRunner;
+
+public class SokobanMain {
+    public static void main(String[] args) {
+        SoloGameRunner gameRunner = new SoloGameRunner();
+        gameRunner.setAgent(Agent.class);
+        gameRunner.setTestCase("test1.json");
+        //System.out.print("___ CHANGEMENT ___");
+        gameRunner.start();
+    }
+}
---
+++ src/test/java/SokobanMain.java
@@ -0,0 +1,12 @@
+import com.codingame.gameengine.runner.SoloGameRunner;
+
+public class SokobanMain {
+    public static void main(String[] args) {
+        /*SoloGameRunner gameRunner = new SoloGameRunner();
+        gameRunner.setAgent(Agent.class);
+        gameRunner.setTestCase("test1.json");
+
+        gameRunner.start();*/
+        System.out.print("___ CHANGEMENT TEST ___");
+    }
+}
